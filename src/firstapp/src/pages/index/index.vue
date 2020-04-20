949fe5c077a9c1a049daf25d1b4535e352d2280a
周悦欣 <zhouyx@foo.com>
2020-04-20 17:38:13 +0800

    首页设计

---
+++ src/firstapp/src/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="shouye">
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'shouye',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
